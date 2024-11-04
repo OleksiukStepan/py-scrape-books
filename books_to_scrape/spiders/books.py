5b8e5b64a497b7af738a0aecd7d439ba5fce6bfd
Stepan <oleksiukstepann@gmail.com>
2024-11-04 23:13:36 +0200

    fix flake8

--- books_to_scrape/spiders/books.py
+++ books_to_scrape/spiders/books.py
@@ -7,8 +7,7 @@ class BooksSpider(scrapy.Spider):
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com/"]
 
-
-    def parse(self, response: Response, **kwargs):
+    def parse(self, response: Response, **kwargs) -> None:
         book_urls = response.css(".image_container > a::attr(href)").getall()
         for book_url in book_urls:
             book_url = response.urljoin(book_url)
@@ -19,10 +18,8 @@ class BooksSpider(scrapy.Spider):
             next_page_url = response.urljoin(next_page)
             yield response.follow(next_page_url, callback=self.parse)
 
-
-    def parse_book(self, response: Response):
+    def parse_book(self, response: Response) -> None:
         rating_map = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
-
         yield {
             "title": response.css(".product_page h1::text").get(),
             "price": float(
